dfcaec2143a7b99dfc4a3d225c59165c60974dfa
Liam Bowers <liambowers718@gmail.com>
2024-05-12 11:08:09 -0700

    Updated routes to adhere to eslint

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
+import bodyParser from 'body-parser';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-import bodyParser from 'body-parser';
 
 /**
  * Injects routes with their handlers to the given Express application.
  * @param {Express} api
  */
 const injectRoutes = (api) => {
-  
   api.use(bodyParser.json());
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
@@ -21,7 +20,6 @@ const injectRoutes = (api) => {
 
   api.post('/users', UsersController.postNew);
   api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
-  };
-
+};
 
 export default injectRoutes;
