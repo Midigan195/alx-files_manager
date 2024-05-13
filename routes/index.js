e46547c0dd5ba0356b68b7e371dac4bbe69ee3b3
Liam Bowers <liambowers718@gmail.com>
2024-05-13 11:39:22 -0700

    Reworked file controller

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
+import bodyParser from 'body-parser';
 import AppController from '../controllers/AppController';
 import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-import bodyParser from 'body-parser';
 
 /**
  * Injects routes with their handlers to the given Express application.
  * @param {Express} api
  */
 const injectRoutes = (api) => {
-  
   api.use(bodyParser.json());
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
@@ -23,14 +22,12 @@ const injectRoutes = (api) => {
   api.post('/users', UsersController.postNew);
   api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
-  api.post('/files', FilesController.postUpload);
-  api.get('/files/:id', FilesController.getShow);
-  api.get('/files', FilesController.getIndex);
-
-  api.put('/files/:id/publish', FilesController.putPublish);
-  api.put('/files/:id/unpublish', FilesController.putUnpublish);
+  api.post('/files', xTokenAuthenticate, FilesController.postUpload);
+  api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
+  api.get('/files', xTokenAuthenticate, FilesController.getIndex);
+  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+  api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
   api.get('/files/:id/data', FilesController.getFile);
-  };
-
+};
 
 export default injectRoutes;
